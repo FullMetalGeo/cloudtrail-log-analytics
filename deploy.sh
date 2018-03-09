49aca97afd1240419a10869f574b0864f71a4041
Grant Soyka <gsoyka@gmail.com>
2018-03-09 09:03:11 -0700

    Updated to python3

---
+++ deploy.sh
@@ -0,0 +1,34 @@
+
+# Set up Variables
+PROFILE="gsoyka-sandbox"
+S3_BUCKET="grant-cf-test"
+STACK_NAME="cloudtrail-elasticsearch"
+
+# Somewhat hacky fix for mac and pip
+case "$(uname -s)" in
+   Darwin)
+      cat << EOF > setup.cfg
+[install]
+prefix=
+EOF
+      ;;
+esac
+
+# Install python requirements
+python -m pip install --upgrade -r requirements.txt -t ./
+
+# Cleanup
+if [[ -f setup.cfg ]]; then rm setup.cfg; fi
+
+aws --profile $PROFILE cloudformation package \
+  --template-file template.yml \
+  --s3-bucket $S3_BUCKET \
+  --output-template-file serverless-output.yml
+
+aws --profile $PROFILE cloudformation deploy \
+  --template-file serverless-output.yml \
+  --stack-name $STACK_NAME \
+  --capabilities CAPABILITY_IAM
+
+echo "Stack $STACK_NAME has been created, but you still need to enter the ElasticSearch password"
+echo 'Create a paramter at $CONFIG_PATH/ES_PASS with the password value'
--- index.py
+++ index.py
@@ -10,9 +10,7 @@ import gzip
 import urllib
 import os
 import traceback
-
-from StringIO import StringIO
-from exceptions import *
+import io
 
 # from awses.connection import AWSConnection
 from elasticsearch import Elasticsearch, RequestsHttpConnection
@@ -23,32 +21,70 @@ logger.setLevel(logging.INFO)
 
 s3 = boto3.client('s3', region_name=os.environ['AWS_REGION'])
 
-awsauth = AWS4Auth(os.environ['AWS_ACCESS_KEY_ID'], os.environ['AWS_SECRET_ACCESS_KEY'], os.environ['AWS_REGION'], 'es', session_token=os.environ['AWS_SESSION_TOKEN'])
-es = Elasticsearch(
-    hosts=[{'host': os.environ['es_host'], 'port': 443}],
-    http_auth=awsauth,
-    use_ssl=True,
-    verify_certs=True,
-    connection_class=RequestsHttpConnection
-)
+# AWS Auth credentials, not currently using, but may some day
+#awsauth = AWS4Auth(os.environ['AWS_ACCESS_KEY_ID'], os.environ['AWS_SECRET_ACCESS_KEY'], os.environ['AWS_REGION'], 'es', session_token=os.environ['AWS_SESSION_TOKEN'])
+
+def get_config():
+    # get SSM client
+    client = boto3.client('ssm', region_name=os.environ['AWS_REGION'])
+    
+    # Get parameters on our path from SSM
+    response = client.get_parameters_by_path(
+        Path = '/dev/CloudTrailPushFunction/',
+        Recursive = False,
+        WithDecryption = True
+    )
+    
+    config = {}
+    for item in response['Parameters']:
+        # Hacky, but trim first 28 characters off name to remove prefix
+        name = item['Name'][28:]
+        config[name] = item['Value']
+    
+    # Uncomment for debugging config
+    #logger.info('Config ' + json.dumps(config, indent=2))
+    
+    return config
     
 def handler(event, context):
+    # Get config from SSM parameter store
+    config = get_config()
+    
+    # Setup elasticsearch connection
+    es = Elasticsearch(
+        hosts=[{'host': config['ES_HOST'], 'port': int(config['ES_PORT'])}],
+        http_auth=(config['ES_USER'], config['ES_PASS']),
+        use_ssl=True,
+        verify_certs=True,
+        connection_class=RequestsHttpConnection
+    )
+    
+    # Log Incoming Message
     logger.info('Event: ' + json.dumps(event, indent=2))
+    message = json.loads(event['Records'][0]['Sns']['Message'])
 
-    s3Bucket = json.loads(event['Records'][0]['Sns']['Message'])['s3Bucket'].encode('utf8')
-    s3ObjectKey = urllib.unquote_plus(json.loads(event['Records'][0]['Sns']['Message'])['s3ObjectKey'][0].encode('utf8'))
-
+    # Define s3 bucket and object
+    s3Bucket = message['s3Bucket']
+    s3ObjectKey = urllib.parse.unquote(message['s3ObjectKey'][0])
+    
+    # Log bucket and object key data
     logger.info('S3 Bucket: ' + s3Bucket)
     logger.info('S3 Object Key: ' + s3ObjectKey)
 
     try:
         response = s3.get_object(Bucket=s3Bucket, Key=s3ObjectKey)
-        content = gzip.GzipFile(fileobj=StringIO(response['Body'].read())).read()
+        
+        # Get compressed file object
+        file = io.BytesIO(response['Body'].read())
+        
+        # Read compressed file
+        content = gzip.GzipFile(fileobj=file).read()
 
+        # Process lines in file
         for record in json.loads(content)['Records']:
             recordJson = json.dumps(record)
             logger.info(recordJson)
-            indexName = 'ct-' + datetime.datetime.now().strftime("%Y-%m-%d")
+            indexName = config['ES_INDEX'] + '-' + datetime.datetime.now().strftime("%Y-%m-%d")
             res = es.index(index=indexName, doc_type='record', id=record['eventID'], body=recordJson)
             logger.info(res)
         return True
@@ -56,4 +92,3 @@ def handler(event, context):
         logger.error('Something went wrong: ' + str(e))
         traceback.print_exc()
         return False
- 
